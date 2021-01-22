0d62f6655e19892575e56af3a7791c0ec9360ace
yoshi <julian404@icloud.com>
2021-01-22 20:47:11 +0100

    Updated logic

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -21,12 +21,14 @@ public class GameController : MonoBehaviour
 {
     private GameField gameField = new GameField();
     public PixelController pixelController;
-
+    private float lastTime;
 
     // Start is called before the first frame update
     void Start()
     {
-       gameField.SetPixelController(pixelController);
+        gameField.SetPixelController(pixelController);
+        gameField.Init();
+        InvokeRepeating("Cycle", 2.0f, 1.0f);
     }
 
     // Update is called once per frame
@@ -35,16 +37,28 @@ public class GameController : MonoBehaviour
         PlayerAction playerAction = ReadPlayerInput();
         gameField.HandlePlayerInput(playerAction);
 
-      // if (speedTimer > currentTick)
-      //  {
-            gameField.Step();
-      //  }
+        // if (speedTimer > currentTick)
+        //  {
+        //  }
         //Update Graphics
         gameField.UpdateDisplay();
     }
 
+    void Cycle()
+    {
+        Debug.Log("Cycle");
+        this.ingameField.Step();
+    }
 
-
+    void FixedUpdate()
+    {
+        if ((lastTime - Time.fixedDeltaTime) > 100000000)
+        {
+            lastTime = Time.fixedDeltaTime;
+            Debug.Log("STEP");
+            //gameField.Step();
+        }
+    }
 
 
     private PlayerAction ReadPlayerInput()
@@ -52,21 +66,25 @@ public class GameController : MonoBehaviour
         PlayerAction selectedAction = new PlayerAction();
         selectedAction = PlayerAction.none;
 
-        if (Input.GetKeyDown("left"))
+        if (Input.GetKeyDown("a"))
         {
             selectedAction = PlayerAction.moveLeft;
         }
-        else if (Input.GetKeyDown("right"))
+        else if (Input.GetKeyDown("d"))
         {
             selectedAction = PlayerAction.moveRight;
         }
-        else if (Input.GetKeyDown("down"))
+        else if (Input.GetKeyDown("s"))
         {
             selectedAction = PlayerAction.moveDown;
         }
-        else if (Input.GetKeyDown("up"))
+        else if (Input.GetKeyDown("w"))
         {
             selectedAction = PlayerAction.drop;
+        }
+        else if (Input.GetKeyDown("q"))
+        {
+            selectedAction = PlayerAction.rotateLeft;
         }
         return selectedAction;
     }
@@ -116,51 +134,167 @@ public class GameField
     class Pixel
     {
         public int x, y;
+        public GamePiece gamePiece = GamePiece.none;
+
         public Pixel(int x, int y)
         {
             this.x = x;
             this.y = y;
         }
+
+        public Pixel(int x, int y, GamePiece gamePiece)
+        {
+            this.x = x;
+            this.y = y;
+            this.gamePiece = gamePiece;
+        }
     }
 
-    List<Pixel> fieldValue = new List<Pixel>();
-    bool[,] currentPiecePosition = new bool[10, 20];
-    List<Pixel> pixelsOfCurrentPiece = new List<Pixel>();
+    int[,] displayPixels = new int[10, 20];
+    List<Pixel> currentPiecePixels = new List<Pixel>();
+
+
+    public void Init()
+    {
+        LoadNewPiece(GamePiece.longI);
+        CopyToDisplayBuffer();
+    }
+
+    private void CopyToDisplayBuffer()
+    {
+        foreach (Pixel pixel in currentPiecePixels)
+        {
+            displayPixels[pixel.x, pixel.y] = (int)pixel.gamePiece;
+        }
+    }
+
+
+    private bool CanMoveDown()
+    {
+        foreach (Pixel pixel in currentPiecePixels)
+        {
+            //Negative Check
+            if (pixel.y == 0)
+            {
+                return false;
+            }
+            if (displayPixels[pixel.x, pixel.y - 1] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     public void Step()
     {
-
-
+        Debug.Log("I am a Step");
+        //Downmovement Possible?
+        if (CanMoveDown())
+        {
+            RemovePixelsFromDisplay(currentPiecePixels);
+            foreach (Pixel pixel in currentPiecePixels)
+            {
+                pixel.y -= 1;
+            }
+        }
+        else
+        {
+            bool isGameOver = false;
+            if (isGameOver)
+            {
+                //GameOverEvent
+            }
+            else
+            {
+                //Store position
+                CopyToDisplayBuffer();
+                LoadNewPiece(GetRandomPiece());
+            }
+        }
     }
+
+    private void RemovePixelsFromDisplay(List<Pixel> pixels)
+    {
+        foreach (Pixel pixel in pixels)
+        {
+            displayPixels[pixel.x, pixel.y] = 0;
+        }
+    }
+
+    private bool CanMoveRight()
+    {
+        foreach (Pixel pixel in currentPiecePixels)
+        {
+            //Negative Check
+            if (pixel.x == 9)
+            {
+                return false;
+            }
+            if (displayPixels[pixel.x + 1, pixel.y] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool CanMoveLeft()
+    {
+        foreach (Pixel pixel in currentPiecePixels)
+        {
+            //Negative Check
+            if (pixel.x == 0)
+            {
+                return false;
+            }
+            if (displayPixels[pixel.x - 1, pixel.y] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     public void HandlePlayerInput(PlayerAction action)
     {
+        Debug.Log($"HandlePlayerInput: {action}");
         if (!MovementAllowed(action))
         {
             return;
         }
 
+        RemovePixelsFromDisplay(currentPiecePixels);
+
         switch (action)
         {
             case PlayerAction.moveDown:
-                foreach (Pixel pixel in pixelsOfCurrentPiece)
-                {
-                    pixel.y -= 1;
-                }
+                Step();
                 break;
             case PlayerAction.moveLeft:
-                foreach (Pixel pixel in pixelsOfCurrentPiece)
+                if (CanMoveLeft())
                 {
-                    pixel.x -= 1;
+                    foreach (Pixel pixel in currentPiecePixels)
+                    {
+                        Debug.Log("Move left");
+                        pixel.x -= 1;
+                    }
                 }
                 break;
             case PlayerAction.moveRight:
-                foreach (Pixel pixel in pixelsOfCurrentPiece)
+                if (CanMoveRight())
                 {
-                    pixel.x += 1;
+                    foreach (Pixel pixel in currentPiecePixels)
+                    {
+                        Debug.Log("Move right");
+                        pixel.x = pixel.x + 1;
+                    }
                 }
                 break;
             case PlayerAction.rotateLeft:
+                Step();
                 Debug.Log("Rotation needs to be implemented!");
                 break;
             case PlayerAction.rotateRight:
@@ -169,34 +303,69 @@ public class GameField
             default:
                 break;
         }
+
+        CopyToDisplayBuffer();
     }
 
-    private GamePiece GetNewRandomPiece()
+    private GamePiece GetRandomPiece()
     {
         int randomIndex = random.Next(0, 8);
         GamePiece newPiece = (GamePiece)randomIndex;
         return newPiece;
     }
 
-    private bool[,] GetStartPositionOfPiece(GamePiece piece)
+    private void LoadNewPiece(GamePiece piece)
     {
-        bool[,] newPiece = new bool[10, 20];
-
+        currentPiecePixels = new List<Pixel>();
 
         //switch(piece)
-        switch (GamePiece.l)
+        switch (piece)
         {
+            case GamePiece.longI:
+                currentPiecePixels.Add(new Pixel(5, 19, piece));
+                currentPiecePixels.Add(new Pixel(5, 18, piece));
+                currentPiecePixels.Add(new Pixel(5, 17, piece));
+                currentPiecePixels.Add(new Pixel(5, 16, piece));
+                break;
+            case GamePiece.square:
+                currentPiecePixels.Add(new Pixel(5, 19, piece));
+                currentPiecePixels.Add(new Pixel(4, 19, piece));
+                currentPiecePixels.Add(new Pixel(5, 18, piece));
+                currentPiecePixels.Add(new Pixel(4, 18, piece));
+                break;
+            case GamePiece.t:
+                currentPiecePixels.Add(new Pixel(5, 19, piece));
+                currentPiecePixels.Add(new Pixel(4, 18, piece));
+                currentPiecePixels.Add(new Pixel(5, 18, piece));
+                currentPiecePixels.Add(new Pixel(6, 18, piece));
+                break;
             case GamePiece.l:
-                newPiece[4, 19] = true;
-                newPiece[4, 18] = true;
-                newPiece[4, 16] = true;
-                newPiece[4, 17] = true;
+                currentPiecePixels.Add(new Pixel(5, 19, piece));
+                currentPiecePixels.Add(new Pixel(5, 18, piece));
+                currentPiecePixels.Add(new Pixel(5, 17, piece));
+                currentPiecePixels.Add(new Pixel(6, 17, piece));
+                break;
+            case GamePiece.mirroredL:
+                currentPiecePixels.Add(new Pixel(5, 19, piece));
+                currentPiecePixels.Add(new Pixel(5, 18, piece));
+                currentPiecePixels.Add(new Pixel(5, 17, piece));
+                currentPiecePixels.Add(new Pixel(4, 17, piece));
+                break;
+            case GamePiece.s:
+                currentPiecePixels.Add(new Pixel(5, 19, piece));
+                currentPiecePixels.Add(new Pixel(6, 19, piece));
+                currentPiecePixels.Add(new Pixel(5, 18, piece));
+                currentPiecePixels.Add(new Pixel(4, 18, piece));
+                break;
+            case GamePiece.mirroredS:
+                currentPiecePixels.Add(new Pixel(4, 19, piece));
+                currentPiecePixels.Add(new Pixel(5, 19, piece));
+                currentPiecePixels.Add(new Pixel(5, 18, piece));
+                currentPiecePixels.Add(new Pixel(6, 18, piece));
                 break;
             default:
                 break;
         }
-        return newPiece;
-
     }
 
     private bool MovementAllowed(PlayerAction desiredAction)
@@ -206,12 +375,35 @@ public class GameField
 
     public void UpdateDisplay()
     {
-        /*
-        foreach(Pixel pixel in fieldValue)
+        for (int col = 0; col < 10; col++)
         {
-            pixelController.SetPixel(pixel.x, pixel.y, Color.blue);
+            for (int row = 0; row < 20; row++)
+            {
+                pixelController.SetPixel(col, row, GetColorByPiece((GamePiece)displayPixels[col, row]));
+            }
         }
-        */
-          pixelController.SetPixel(2, 2, Color.blue);
+    }
+
+    private Color GetColorByPiece(GamePiece gamePiece)
+    {
+        switch (gamePiece)
+        {
+            case GamePiece.none:
+                return Color.grey;
+            case GamePiece.l:
+                return Color.blue;
+            case GamePiece.longI:
+                return Color.cyan;
+            case GamePiece.mirroredL:
+                return new Color(1, 0.65f, 0, 1);
+            case GamePiece.t:
+                return new Color(0.5f, 0, 1, 1);
+            case GamePiece.mirroredS:
+                return Color.green;
+            case GamePiece.s:
+                return Color.red;
+            default:
+                return Color.white;
+        }
     }
 }
