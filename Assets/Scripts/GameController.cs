f96d0cb0921ec1be82bb1692c1727e97649cf654
yoshi <julian404@icloud.com>
2021-01-29 12:08:20 +0100

    Added Namespace for StatisticManager

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,185 +2,183 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System; //random function
 
-public enum PlayerAction
+namespace TetrisGame
 {
-    none,
-    moveLeft,
-    moveRight,
-    moveDown,
-    drop,
-    rotateLeft,
-    rotateRight,
-    store
-}
-
-
-public class GameController : MonoBehaviour
-{
-    public GameField gameField;
-
-    public Text storedPieceText;
-    public Text scoreText;
-
-    public PixelController pixelController;
-    PlayerAction currentPlayerAction = PlayerAction.none;
-    PlayerAction lastPlayerAction = PlayerAction.none;
-
-    // Handling player Input
-    bool isSameActionAsBefore = false;
-    ActionTimeCounter inputDelayTimer = new ActionTimeCounter(0.1f);
-    ActionTimeCounter inputCycleTimer = new ActionTimeCounter(0.05f);
-
-    // Handling game speed
-    ActionTimeCounter pushDownTimer = new ActionTimeCounter(1f);
-
-    // Start is called before the first frame update
-    void Start()
+    public enum PlayerAction
     {
-        gameField.SetPixelController(pixelController);
-        gameField.Init();
+        none,
+        moveLeft,
+        moveRight,
+        moveDown,
+        drop,
+        rotateLeft,
+        rotateRight,
+        store
     }
 
-    // Update is called once per frame
-    void Update()
+    public class GameController : MonoBehaviour
     {
-        if (!gameField.isGameOver)
+        private GameField gameField = new GameField();
+
+        public Text storedPieceText;
+        public Text scoreText;
+
+        public PixelController pixelController;
+        PlayerAction currentPlayerAction = PlayerAction.none;
+        PlayerAction lastPlayerAction = PlayerAction.none;
+
+        // Handling player Input
+        bool isSameActionAsBefore = false;
+        ActionTimeCounter inputDelayTimer = new ActionTimeCounter(0.1f);
+        ActionTimeCounter inputCycleTimer = new ActionTimeCounter(0.05f);
+
+        // Handling game speed
+        ActionTimeCounter pushDownTimer = new ActionTimeCounter(1f);
+
+        // Start is called before the first frame update
+        void Start()
         {
-            currentPlayerAction = ReadPlayerInput();
+            gameField.SetPixelController(pixelController);
+            gameField.Init();
+        }
 
-            if (currentPlayerAction != lastPlayerAction)
+        // Update is called once per frame
+        void Update()
+        {
+            if (!gameField.isGameOver)
             {
-                isSameActionAsBefore = false;
-            }
+                currentPlayerAction = ReadPlayerInput();
 
-            //Increase timers.
-            pushDownTimer.AddTime(Time.deltaTime);
-            inputDelayTimer.AddTime(Time.deltaTime);
-            inputCycleTimer.AddTime(Time.deltaTime);
-
-
-            if (isSameActionAsBefore)
-            {
-                if (inputDelayTimer.IsReached())
+                if (currentPlayerAction != lastPlayerAction)
                 {
-                    if (inputCycleTimer.IsReached())
+                    isSameActionAsBefore = false;
+                }
+
+                //Increase timers.
+                pushDownTimer.AddTime(Time.deltaTime);
+                inputDelayTimer.AddTime(Time.deltaTime);
+                inputCycleTimer.AddTime(Time.deltaTime);
+
+
+                if (isSameActionAsBefore)
+                {
+                    if (inputDelayTimer.IsReached())
+                    {
+                        if (inputCycleTimer.IsReached())
+                        {
+                            inputCycleTimer.Clear();
+                            gameField.HandlePlayerInput(currentPlayerAction);
+                        }
+                    }
+                    else
                     {
                         inputCycleTimer.Clear();
-                        gameField.HandlePlayerInput(currentPlayerAction);
                     }
                 }
                 else
                 {
-                    inputCycleTimer.Clear();
+                    gameField.HandlePlayerInput(currentPlayerAction);
+                    inputDelayTimer.Clear();
+                    isSameActionAsBefore = true;
                 }
+
+                if (pushDownTimer.IsReached())
+                {
+                    pushDownTimer.Clear();
+                    gameField.Push();
+                }
+
+                gameField.CalculatePreviewPixels();
+                gameField.UpdateDisplay();
+
+                lastPlayerAction = currentPlayerAction;
             }
             else
             {
-                gameField.HandlePlayerInput(currentPlayerAction);
-                inputDelayTimer.Clear();
-                isSameActionAsBefore = true;
+                gameField.SetGameOverScreen();
             }
+        }
 
-            if (pushDownTimer.IsReached())
+        void FixedUpdate()
+        {
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            //Hold
+            storedPieceText.text = gameField.storedPiece.ToString();
+
+            //Score
+            scoreText.text = gameField.score.ToString();
+        }
+
+        private PlayerAction ReadPlayerInput()
+        {
+            PlayerAction selectedAction = new PlayerAction();
+            selectedAction = PlayerAction.none;
+
+            if (Input.GetKey("a"))
             {
-                pushDownTimer.Clear();
-                gameField.Push();
+                selectedAction = PlayerAction.moveLeft;
+            }
+            else if (Input.GetKey("d"))
+            {
+                selectedAction = PlayerAction.moveRight;
+            }
+            else if (Input.GetKey("s"))
+            {
+                selectedAction = PlayerAction.moveDown;
+            }
+            else if (Input.GetKeyDown("w"))
+            {
+                selectedAction = PlayerAction.drop;
+            }
+            else if (Input.GetKeyDown("q"))
+            {
+                selectedAction = PlayerAction.rotateLeft;
+            }
+            else if (Input.GetKeyDown("e"))
+            {
+                selectedAction = PlayerAction.rotateRight;
+            }
+            else if (Input.GetKeyDown("space"))
+            {
+                selectedAction = PlayerAction.store;
+            }
+            return selectedAction;
+        }
+
+        private class ActionTimeCounter
+        {
+            float timer;
+            float threshold = 0;
+
+            public ActionTimeCounter(float threshold)
+            {
+                this.threshold = threshold;
             }
 
-            gameField.CalculatePreviewPixels();
-            gameField.UpdateDisplay();
+            public void AddTime(float time)
+            {
+                timer += time;
+            }
 
-            lastPlayerAction = currentPlayerAction;
-        }
-        else
-        {
-            gameField.SetGameOverScreen();
-        }
-    }
+            public void SetThreshold(float time)
+            {
+                threshold = time;
+            }
 
-    void FixedUpdate()
-    {
-        UpdateUI();
-    }
+            public bool IsReached()
+            {
+                return timer > threshold;
+            }
 
-    private void UpdateUI()
-    {
-        //Hold
-        storedPieceText.text = gameField.storedPiece.ToString();
-
-        //Score
-        scoreText.text = gameField.score.ToString();
-    }
-
-    private PlayerAction ReadPlayerInput()
-    {
-        PlayerAction selectedAction = new PlayerAction();
-        selectedAction = PlayerAction.none;
-
-        if (Input.GetKey("a"))
-        {
-            selectedAction = PlayerAction.moveLeft;
-        }
-        else if (Input.GetKey("d"))
-        {
-            selectedAction = PlayerAction.moveRight;
-        }
-        else if (Input.GetKey("s"))
-        {
-            selectedAction = PlayerAction.moveDown;
-        }
-        else if (Input.GetKeyDown("w"))
-        {
-            selectedAction = PlayerAction.drop;
-        }
-        else if (Input.GetKeyDown("q"))
-        {
-            selectedAction = PlayerAction.rotateLeft;
-        }
-        else if (Input.GetKeyDown("e"))
-        {
-            selectedAction = PlayerAction.rotateRight;
-        }
-        else if (Input.GetKeyDown("space"))
-        {
-            selectedAction = PlayerAction.store;
-        }
-        return selectedAction;
-    }
-
-    private class ActionTimeCounter
-    {
-        float timer;
-        float threshold = 0;
-
-        public ActionTimeCounter(float threshold)
-        {
-            this.threshold = threshold;
-        }
-
-        public void AddTime(float time)
-        {
-            timer += time;
-        }
-
-        public void SetThreshold(float time)
-        {
-            threshold = time;
-        }
-
-        public bool IsReached()
-        {
-            return timer > threshold;
-        }
-
-        public void Clear()
-        {
-            timer = 0f;
+            public void Clear()
+            {
+                timer = 0f;
+            }
         }
     }
 }
-
-
-
