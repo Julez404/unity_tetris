eecc7f9bcea07f038a7bc15f103574fb0da23837
yoshi <julian404@icloud.com>
2021-01-29 10:43:36 +0100

    Added StatisticManager file

--- Assets/Scripts/StatisticManager.cs
+++ Assets/Scripts/StatisticManager.cs
@@ -1,10 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using GamePiece;
+
 
 public class StatisticManager : MonoBehaviour
 {
 
+//ToDo
+/*
+Tetris Rate (percentage of Lines cleared with tetris)
+I-Piece counter
+Lines cleared
+Drought counter (number of pieces since last I-piece)
+Burn counter (number of lines cleared without tetris since last tetris)
+*/
 
+
+
+/*
+public void AddPieceToStatistic(GamePiece)
+{
+
+}
+*/
 
 }
